4aa7d4dd804a15a2d0a5d056a9edba72f4fa6f39
Scott M. Wyant <29161635+scottmwyant@users.noreply.github.com>
2020-08-29 23:00:19 -0400

    Minor refactoring of the activate method

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,4 +5,4 @@ import { toggleComment } from './toggleComment';
 // Create an array where each item  is a function that returns 'void'.
 // Since these functions will be registered as commands, the names of the
 // functions must match what is defined in the project's package.json.
-export const commands: (() => void)[] = [addLineNumbers, removeLineNumbers, toggleComment];
+export const gcodeCommands: (() => void)[] = [addLineNumbers, removeLineNumbers, toggleComment];
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
-import * as vscode from 'vscode';
-import { commands } from './commands'
+import { commands, ExtensionContext } from 'vscode';
+import { gcodeCommands } from './commands'
 
 /**
  * Register each item in an array of functions as a command.
  * Use the name of the function as the unique identifier for
  * the command.
  */
-export function activate(context: vscode.ExtensionContext) {
-	commands.forEach(cmd => {
+export function activate(context: ExtensionContext) {
+	gcodeCommands.forEach(cmd => {
 		context.subscriptions.push(
-			vscode.commands.registerCommand(`gcode.${cmd.name}`, cmd)
+			commands.registerCommand(`gcode.${cmd.name}`, cmd)
 		);
 	});
 }
