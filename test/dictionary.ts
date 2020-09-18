05700dabc7d5e397d97bb4990dba3fef163b7c60
Scott M. Wyant <29161635+scottmwyant@users.noreply.github.com>
2020-09-17 22:20:02 -0400

    Rename test.ts to dictionary.ts, add tests

--- test/dictionary.ts
+++ test/dictionary.ts
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import { Dictionary } from '../src/hoverProvider/dictionary';
 
-describe('Describe ability to parse comments in hoverProvider/dictionary.ts', function(){
+describe('Describe ability to parse inline definitions from comments', function(){
     const data = [
         {
             "text": "(G04 = Dwell)",
@@ -26,10 +26,22 @@ describe('Describe ability to parse comments in hoverProvider/dictionary.ts', fu
             "obj": {"M1": "Program stop, optional"},
             "find": "VC33",
             "expected": "Tool number"
+        },
+        {
+            "text": "(G01 = feed: linear)",
+            "obj": {"M1": "Program stop, optional"},
+            "find": "G01",
+            "expected": undefined
+        },
+        {
+            "text": "(G01 - Feed)",
+            "obj": {"M1": "Program stop, optional"},
+            "find": "G01",
+            "expected": undefined
         }
     ].forEach(item => {
 
-        it(item.text, function(){
+        it(`${item.text} = ${item.expected}`, function(){
             assert.equal(new Dictionary(item.obj, item.text).lookup(item.find), item.expected);
         });
 
