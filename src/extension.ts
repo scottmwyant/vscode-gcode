16a46acc2b6c8342020afc0ca4525b0f04812002
Scott M. Wyant <29161635+scottmwyant@users.noreply.github.com>
2021-02-06 07:29:28 -0500

    testing

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import { ExtensionContext } from 'vscode';
-import * as commands from './commands'
-import * as hoverProvider from './hoverProvider'
-import * as grammar from './grammar'
+// import * as commands from './commands'
+// import * as hoverProvider from './hoverProvider'
+// import * as grammar from './grammar'
+import * as decoration from './decoration'
 
 export function activate(context: ExtensionContext) {
-    
-    commands.register(context);
-    hoverProvider.register(context);
-    grammar.register(context);
+
+    // commands.register(context);
+    // hoverProvider.register(context);
+    // grammar.register(context);
+    decoration.register(context);
 }
